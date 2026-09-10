4b4928620ef94eae3b62edaf2df0334c10e1200e
gabygimenes <gabriela.gimenes@edu.senai.br>
2026-09-10 11:56:19 -0300

    nível1

---
+++ nível-1.js
@@ -0,0 +1,51 @@
+//ex1
+const aluno = {
+  nome: "Gabriela",
+  idade: 16,
+  curso: "Desenvolvimento de Sistemas"
+};
+console.log(`Nome: ${aluno.nome}`)
+console.log(`Nome: ${aluno.idade}`)
+console.log(`Nome: ${aluno.curso}`)
+
+
+
+//ex2
+const produto = {
+  nome: "Teclado",
+  preco: 150,
+  marca: "Logitech"
+};
+
+console.log(produto.nome)
+console.log(produto.preco)
+console.log(produto.marca)
+
+//ex3
+const nome = "Carlos";
+const idade = 18;
+
+console.log(` Carlos tem ${idade} anos`)
+
+//ex4
+
+const jogo = {
+  nome: "Minecraft",
+  preco: 99
+};
+ console.log (`O jogo ${jogo.nome} custa R$ ${jogo.preco}`)
+
+ //ex5
+ const dobro = (numero) => numero * 2
+ console.log(dobro(5));
+
+ //ex6
+ const somar = (a,b) => a + b; 
+ console.log(somar(10, 20));
+
+ //ex7
+ const apresentar = {
+     nome: "Gabriela",
+     curso: "DS"
+ }
+ console.log(`Olá, meu nome é ${apresentar.nome} e estudo ${apresentar.curso}`);
